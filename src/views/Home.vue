2e40b076e2cc2fb67a6ce18128aefcc7201103c8
jer s <jrswanson4@wisc.edu>
2021-11-01 21:42:16 -0500

    Adjusted nav bar to be a v-toolbar to allow for better functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <v-form>
-    <v-bottom-navigation color="white" style="background-color: #fd6359" grow height="50px">
+    <!-- <v-bottom-navigation color="white" style="background-color: #fd6359" grow height="50px" width="3000px"> 
       <v-btn style="text-color: white; background-color: #fd6359" height="50px">
         <span>Home</span>
         <v-icon>mdi-home</v-icon>
@@ -21,9 +21,42 @@
         <span>Bookmarks</span>
         <v-icon>mdi-bookmark</v-icon>
       </v-btn>
-    </v-bottom-navigation>
+    </v-bottom-navigation>  --> 
+    <div>
+    <v-toolbar
+      dark
+      dense
+      src="https://i.imgur.com/yicLpLZ.jpg" 
+    >
+      
+
+      
+      <v-spacer></v-spacer>
+      <v-btn  icon> 
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+      <v-btn  icon>
+        <v-icon>mdi-earth</v-icon>
+      </v-btn>
+      <v-btn  icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-food</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-bookmark</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+      
+    </v-toolbar>
+  </div>
+    
     <v-col class="pt-15">
-      <h1 align="center" style="color: #7d312c">
+      <!-- <h1 align="center" style="color: #7d312c">
         Welcome to The Recipe Compiler!
       </h1>
       <h3 align="center" style="color: #7d312c">
@@ -40,7 +73,7 @@
           >
         </v-col>
         <v-col cols="5"></v-col>
-      </v-row>
+      </v-row> -->
       <v-row class="mt-4 mb-4">
         <v-col cols="4">
           <v-card class="mt-4 mb-4" height="300" shaped elevation="4">
