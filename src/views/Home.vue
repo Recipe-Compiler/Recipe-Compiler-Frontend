ef98a531bb437488d66250995b369d26db691fc1
jer s <jrswanson4@wisc.edu>
2021-11-02 21:57:19 -0500

    adjusted home screen functionality and cleaned up UI design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,77 +21,168 @@
         <span>Bookmarks</span>
         <v-icon>mdi-bookmark</v-icon>
       </v-btn>
-    </v-bottom-navigation>  --> 
+    </v-bottom-navigation>  -->
     <div>
-    <v-toolbar
-      dark
-      dense
-      src="https://i.imgur.com/yicLpLZ.jpg" 
-    >
-      
+      <v-toolbar dark dense src="https://i.imgur.com/yicLpLZ.jpg">
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-earth</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-food</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-bookmark</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </div>
 
-      
-      <v-spacer></v-spacer>
-      <v-btn  icon> 
-        <v-icon>mdi-home</v-icon>
-      </v-btn>
-      <v-btn  icon>
-        <v-icon>mdi-earth</v-icon>
-      </v-btn>
-      <v-btn  icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-food</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-bookmark</v-icon>
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-account</v-icon>
-      </v-btn>
-      
-    </v-toolbar>
-  </div>
-  <v-carousel v-model="model">
-    <v-carousel-item
-      v-for="(color, i) in colors"
-      :key="color"
+    <v-carousel
+      cycle
+      show-arrows-on-hover
+      hide-delimiter-background
+      height="1000px"
     >
-      <v-sheet
-        :color="color"
-        height="100%"
-        title
-      >
-        <v-row
-          class="fill=height"
-          align="center"
-          justify="center"
-        >
-         <div class="text-h2">
-           Slide {{ i + 1 }}
-         </div>
-        </v-row>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>        
-   <v-col class="pt-15">
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
+    </v-carousel>
+
+    <v-col class="pt-15">
       <v-row class="mt-4 mb-4">
-        <v-col cols="4">
-          <v-card class="mt-4 mb-4" height="300" shaped elevation="4">
-            <span>Recipe 1</span>
-          </v-card>
+        <v-col cols="3">
+          <v-hover>
+            <template v-slot:default="{ hover }">
+              <v-card class="mx-auto" max-width="344" height="500">
+                <v-card-text>
+                  <h2 class="text-h6 primary--text">Butternut Squash Bisque</h2>
+                  A Delicious fall soup that will fend off the coming cold!
+                </v-card-text>
+
+                <v-card-title>
+                  <v-rating
+                    :value="4"
+                    dense
+                    color="orange"
+                    background-color="orange"
+                    hover
+                    class="mr-2"
+                  ></v-rating>
+                  <span class="primary--text text-subtitle-2">64 Reviews</span>
+                </v-card-title>
+
+                <v-fade-transition>
+                  <v-overlay v-if="hover" absolute color="#036358">
+                    <v-btn>See more info</v-btn>
+                  </v-overlay>
+                </v-fade-transition>
+              </v-card>
+            </template>
+          </v-hover>
         </v-col>
-        <v-col cols="4">
-          <v-card class="mt-4 mb-4" height="300" shaped elevation="4">
-            <span>Recipe 2</span>
-          </v-card>
+        <v-col cols="3">
+          <v-hover>
+            <template v-slot:default="{ hover }">
+              <v-card class="mx-auto" max-width="344" height="500">
+                <v-card-text>
+                  <h2 class="text-h6 primary--text">Pumkin Pie</h2>
+                  A Delicious fall treat that pairs well with whipped cream or vanilla ice cream!
+                </v-card-text>
+
+                <v-card-title>
+                  <v-rating
+                    :value="4"
+                    dense
+                    color="orange"
+                    background-color="orange"
+                    hover
+                    class="mr-2"
+                  ></v-rating>
+                  <span class="primary--text text-subtitle-2">64 Reviews</span>
+                </v-card-title>
+
+                <v-fade-transition>
+                  <v-overlay v-if="hover" absolute color="#036358">
+                    <v-btn>See more info</v-btn>
+                  </v-overlay>
+                </v-fade-transition>
+              </v-card>
+            </template>
+          </v-hover>
         </v-col>
-        <v-col cols="4">
-          <v-card class="mt-4 mb-4" height="300" shaped elevation="4">
-            <span>Recipe 3</span>
-          </v-card>
+        <v-col cols="3">
+          <v-hover>
+            <template v-slot:default="{ hover }">
+              <v-card class="mx-auto" max-width="344" height="500">
+                <v-card-text>
+                  <h2 class="text-h6 primary--text">Pesto Pasta</h2>
+                  A simple, comforting pasta dish for fall lovers! 
+                </v-card-text>
+
+                <v-card-title>
+                  <v-rating
+                    :value="4"
+                    dense
+                    color="orange"
+                    background-color="orange"
+                    hover
+                    class="mr-2"
+                  ></v-rating>
+                  <span class="primary--text text-subtitle-2">64 Reviews</span>
+                </v-card-title>
+
+                <v-fade-transition>
+                  <v-overlay v-if="hover" absolute color="#036358">
+                    <v-btn>See more info</v-btn>
+                  </v-overlay>
+                </v-fade-transition>
+              </v-card>
+            </template>
+          </v-hover>
+        </v-col>
+        <v-col cols="3">
+          <v-hover>
+            <template v-slot:default="{ hover }">
+              <v-card class="mx-auto" max-width="344" height="500">
+                <v-card-text>
+                  <h2 class="text-h6 primary--text">Braised Short Ribs</h2>
+                  A Hearty stew that can be made in any situation! 
+                </v-card-text>
+
+                <v-card-title>
+                  <v-rating
+                    :value="4"
+                    dense
+                    color="orange"
+                    background-color="orange"
+                    hover
+                    class="mr-2"
+                  ></v-rating>
+                  <span class="primary--text text-subtitle-2">64 Reviews</span>
+                </v-card-title>
+
+                <v-fade-transition>
+                  <v-overlay v-if="hover" absolute color="#036358">
+                    <v-btn>See more info</v-btn>
+                  </v-overlay>
+                </v-fade-transition>
+              </v-card>
+            </template>
+          </v-hover>
         </v-col>
       </v-row>
     </v-col>
@@ -105,18 +196,24 @@ export default Vue.extend({});
 </script>
 <script>
 export default {
-    data: () => ({
-      model: 0,
-      colors: [
-        'primary',
-        'secondary',
-        'yellow darken-2',
-        'red',
-        'orange',
+  data() {
+    return {
+      items: [
+        {
+          src: "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/11/Roasted-Butternut-Squash-Soup-1-2.jpg",
+        },
+        {
+          src: "https://d1uz88p17r663j.cloudfront.net/original/b829edd68c818352a26f754a8184e636_bak---02---libby_s-famous-pumpkin-pie-617_edit.jpg",
+        },
+        {
+          src: "https://www.seriouseats.com/thmb/G5vpQw2SVCP4DBBFKVhUW0tXsvU=/1500x1125/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2016__02__20160209-finishing-pasta-pesto-vicky-wasik-8-2-ef92adeced95462b8a8871aec7e163e8.jpg",
+        },
+        {
+          src: "https://assets.bonappetit.com/photos/5c4b21e785b9bd2cf3b17454/16:9/w_4800,h_2700,c_limit/red-wine-braised-short-ribs.jpg",
+        },
       ],
-    }), 
-  }
-</script>
-
-
+    };
+  },
+};
+</script>  ss
 <style></style>
