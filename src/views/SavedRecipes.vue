9652c8c80c8a3e833df7d20f0266cf8d35c99fd5
joshvCS <jtvillanueva@wisc.edu>
2021-11-04 22:36:05 -0500

    Structure Change / V-List Added

--- src/views/SavedRecipes.vue
+++ src/views/SavedRecipes.vue
@@ -26,300 +26,26 @@
       <v-row align="center" justify="center">
         <h1>Saved Recipes</h1>
       </v-row>
-    <v-row class="mt-4 mb-4">
-        <v-col cols="4">
-            <v-card :loading="loading" class="mt-4 mb-4" max-width="400" max-height="455">
-            <template slot="progress">
-            <v-progress-linear color="deep-purple" height="10" indeterminate>
-            </v-progress-linear>
-            </template>
-                <v-img
-                height="250"
-                src="https://www.cooking-therapy.com/wp-content/uploads/2021/08/Matcha-Cookies-6.jpg"
-                ></v-img>
-            <v-card-title>Matcha Cookies</v-card-title>
-
-        <v-card-text>
-        <v-row align="center" class="mx-0">
-            <v-rating
-            :value="4.5"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="14"
-            ></v-rating>
-
-            <div class="grey--text ms-4">
-            4.5 (413)
-            </div>
-        </v-row>
-
-        <div class="my-4 text-subtitle-1">
-            2 Hours 35 mins • Dessert, Easy
-        </div>
-
-        <div>Add info about the recipe.</div>
-        </v-card-text>
-
-        <v-divider class="mx-4"></v-divider>
-        <v-card-actions>
-        <v-btn
-            color="red"
-            text
-            @click="unsaveRecipe"
-        >
-            Unsave
-        </v-btn>
-        </v-card-actions>
-        </v-card>
-
-        <v-card :loading="loading" class="mt-4 mb-4" max-width="400" max-height="455">
-            <template slot="progress">
-            <v-progress-linear color="deep-purple" height="10" indeterminate>
-            </v-progress-linear>
-            </template>
-                <v-img
-                height="250"
-                src="https://www.justonecookbook.com/wp-content/uploads/2017/07/Spicy-Shoyu-Ramen-NEW-500x375.jpg"
-                ></v-img>
-            <v-card-title>Homemade Shoyu Ramen</v-card-title>
-
-        <v-card-text>
-        <v-row align="center" class="mx-0">
-            <v-rating
-            :value="4.2"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="14"
-            ></v-rating>
-
-            <div class="grey--text ms-4">
-            4.2 (413)
-            </div>
-        </v-row>
-
-        <div class="my-4 text-subtitle-1">
-            6 Hours 30 mins • Ramen, Pork, Soup
-        </div>
-
-        <div>Add info about the recipe.</div>
-        </v-card-text>
-
-        <v-divider class="mx-4"></v-divider>
-        <v-card-actions>
-        <v-btn
-            color="red"
-            text
-            @click="unsaveRecipe"
-        >
-            Unsave
-        </v-btn>
-        </v-card-actions>
-        </v-card>
-    </v-col>
-
-    <v-col cols="4">
-            <v-card :loading="loading" class="mt-4 mb-4" max-width="400" max-height="455">
-            <template slot="progress">
-            <v-progress-linear color="deep-purple" height="10" indeterminate>
-            </v-progress-linear>
-            </template>
-                <v-img
-                height="250"
-                src="https://www.lovebakesgoodcakes.com/wp-content/uploads/2013/06/Shrimp-Scampi-square.jpg"
-                ></v-img>
-            <v-card-title>Classic Shrimp Scampi</v-card-title>
-
-        <v-card-text>
-        <v-row align="center" class="mx-0">
-            <v-rating
-            :value="5"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="14"
-            ></v-rating>
-
-            <div class="grey--text ms-4">
-            5 (400)
-            </div>
-        </v-row>
-
-        <div class="my-4 text-subtitle-1">
-            15 mins • Quick, Shrimp, Easy
-        </div>
-
-        <div>Add info about the recipe.</div>
-        </v-card-text>
-
-        <v-divider class="mx-4"></v-divider>
-        <v-card-actions>
-        <v-btn
-            color="red"
-            text
-            @click="unsaveRecipe"
-        >
-            Unsave
-        </v-btn>
-        </v-card-actions>
-        </v-card>
-
-        <v-card :loading="loading" class="mt-4 mb-4" max-width="400" max-height="455">
-            <template slot="progress">
-            <v-progress-linear color="deep-purple" height="10" indeterminate>
-            </v-progress-linear>
-            </template>
-                <v-img
-                height="250"
-                src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2019%2F08%2F26232540%2F5177935.jpg&q=85"
-                ></v-img>
-            <v-card-title>Coconut Cream Pie</v-card-title>
-
-        <v-card-text>
-        <v-row align="center" class="mx-0">
-            <v-rating
-            :value="4.7"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="14"
-            ></v-rating>
-
-            <div class="grey--text ms-4">
-            4.7 (400)
-            </div>
-        </v-row>
-
-        <div class="my-4 text-subtitle-1">
-            4 Hours 50 Mins • Pie, Dessert 
-        </div>
-
-        <div>Add info about the recipe.</div>
-        </v-card-text>
-
-        <v-divider class="mx-4"></v-divider>
-        <v-card-actions>
-        <v-btn
-            color="red"
-            text
-            @click="unsaveRecipe"
-        >
-            Unsave
-        </v-btn>
-        </v-card-actions>
-        </v-card>
-    </v-col>
-
-    <v-col cols="4">
-            <v-card :loading="loading" class="mt-4 mb-4" max-width="400" max-height="455">
-            <template slot="progress">
-            <v-progress-linear color="deep-purple" height="10" indeterminate>
-            </v-progress-linear>
-            </template>
-                <v-img
-                height="250"
-                src="https://cook.me/wp-content/uploads/2020/03/Spiced-carrot-lentil-soup-Recipe-How-To-Make-Spiced-carrot-lentil-soup-Delicious-Spiced-carrot-lentil-soup-8-678x1024.jpg"
-                ></v-img>
-            <v-card-title>Spiced Carrot & Lentil Soup</v-card-title>
-
-        <v-card-text>
-        <v-row align="center" class="mx-0">
-            <v-rating
-            :value="5"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="14"
-            ></v-rating>
-
-            <div class="grey--text ms-4">
-            5 (400)
-            </div>
-        </v-row>
-
-        <div class="my-4 text-subtitle-1">
-            25 mins • Quick, Soup, Lentil
-        </div>
-
-        <div>Add info about the recipe.</div>
-        </v-card-text>
-
-        <v-divider class="mx-4"></v-divider>
-        <v-card-actions>
-        <v-btn
-            color="red"
-            text
-            @click="unsaveRecipe"
-        >
-            Unsave
-        </v-btn>
-        </v-card-actions>
-        </v-card>
-
-        <v-card :loading="loading" class="mt-4 mb-4" max-width="400" max-height="455">
-            <template slot="progress">
-            <v-progress-linear color="deep-purple" height="10" indeterminate>
-            </v-progress-linear>
-            </template>
-                <v-img
-                height="250"
-                src="https://miro.medium.com/max/1400/1*YtnKAZ3HapFN_82nqHEeIw.jpeg"
-                ></v-img>
-            <v-card-title>Butter Zopf - Swiss Braided Bread</v-card-title>
-
-        <v-card-text>
-        <v-row align="center" class="mx-0">
-            <v-rating
-            :value="4.1"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="14"
-            ></v-rating>
-
-            <div class="grey--text ms-4">
-            4.1 (400)
-            </div>
-        </v-row>
-
-        <div class="my-4 text-subtitle-1">
-            3 Hours 5 mins • Bread, Swiss, Bake
-        </div>
-
-        <div>Add info about the recipe.</div>
-        </v-card-text>
-
-        <v-divider class="mx-4"></v-divider>
-        <v-card-actions>
-        <v-btn
-            color="red"
-            text
-            @click="unsaveRecipe"
-        >
-            Unsave
-        </v-btn>
-        </v-card-actions>
-        </v-card>
-    </v-col>
-    
-    <v-row align="center" justify="center">
+      <v-row align="center" justify="center">
       <v-btn
-        color="secondary"
+        color="#fd6359"
         elevation="2"
-        @click="loadMore"
+        @click="setFilters"
       >
-        Load More Saved Recipes
+        Set Filters
       </v-btn>
     </v-row>
-    
-    </v-row> 
+    <v-col class="pt-15">
+        <v-row align="center" justify="center">
+        <v-data-table align="center" justify="center"
+            :headers="tableHeaders"
+            :items="savedRecipes"
+            :items-per-page="5"
+            class="elevation-1"
+            >
+            </v-data-table>
+        </v-row>
+    </v-col>
   </v-col>
   </v-form>
 </template>
@@ -327,21 +53,41 @@
 <script lang="ts">
 import Vue from "vue";
 import { mapActions } from "vuex";
+import { api } from "@/App.vue";
 export default Vue.extend({
+    data() {
+        return {
+            tableHeaders: [
+                {
+                    text: "Recipes"
+                },
+            ],
+            savedRecipes: [],
+        }
+    },
+    mounted() {
+        let user = JSON.parse(localStorage.user);
+        this.username = user.username;
+    },
     methods: {
         goHome() {
             this.$router.push("Home");
+        },
+        setFilters() {
+            // sets filters to display recipes
+            return 0;
         },
         viewRecipe() {
           // route to the correct recipe page
           return 0;
         },
         unsaveRecipe() {
-          // removes recipe from saved list
+          // removes recipe from user's saved list
           return 0;
         },
-        loadMore() {
-          // loads more recipes
+        getSavedRecipes() {
+          api
+            .get(`RecipeCompiler/${this.username}/saved`)
           return 0;
         }
      },
