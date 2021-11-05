65f581e7927e57abc7c817be4b7a869c366c4914
achermack <achermack@wisc.edu>
2021-11-05 13:33:46 -0500

    feat: rework carousel

---
+++ src/components/RecipeCarousel.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card>
+    <v-carousel
+      cycle
+      show-arrows-on-hover
+      hide-delimiter-background
+      height="1250px"
+      v-model="recipeIndex"
+    >
+      <v-carousel-item
+        v-for="(item, i) in recipes"
+        :key="i"
+        :src="item.imageUrl"
+        fade
+      ></v-carousel-item>
+    </v-carousel>
+    <v-card-title>
+      {{ recipes[recipeIndex].name }}
+    </v-card-title>
+  </v-card>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    recipes: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      recipeIndex: 0,
+    };
+  },
+});
+</script>
